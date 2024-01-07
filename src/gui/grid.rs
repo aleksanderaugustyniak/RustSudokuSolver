603b26e21c5281087ce6ba83513d9c49f35de856
Aleksander Augustyniak <aleksander.z.augustyniak@gmail.com>
2024-01-07 23:01:59 +0100

    Prepare highlighting number on the play grid

--- src/gui/grid.rs
+++ src/gui/grid.rs
@@ -5,10 +5,14 @@ use std::rc::Rc;
 const GRID_SIZE: usize = 9;
 const BUTTON_SIZE: i32 = 50;
 const GRID_OFFSET_FROM_LEFT: i32 = 30;
+const LIGHT_BUTTON_COLOR: Color = Color::from_rgb(200, 200, 200);
+const DARK_BUTTON_COLOR: Color = Color::from_rgb(150, 150, 150);
+const HIGHLIGHTED_BUTTON_COLOR: Color = Color::from_rgb(100, 100, 250);
 
 pub struct Grid {
     current_number: Rc<RefCell<String>>,
     control_panel: Rc<RefCell<[Button; GRID_SIZE]>>,
+    play_grid: Rc<RefCell<[[Button; GRID_SIZE]; GRID_SIZE]>>,
 }
 
 impl Grid{
@@ -16,6 +20,7 @@ impl Grid{
         Grid {
             current_number: Rc::new(RefCell::new("1".to_string())),
             control_panel: Default::default(),
+            play_grid: Default::default(),
         }
     }
 
@@ -23,21 +28,8 @@ impl Grid{
         let app = app::App::default();
         let mut window = window::Window::new(100, 100, 600, 600, "Sudoku");
         window.set_color(Color::White);
-        let mut grid = Pack::new(
-            10,
-            10,
-            BUTTON_SIZE * GRID_SIZE as i32,
-            BUTTON_SIZE * GRID_SIZE as i32,
-            "");
-            grid.make_resizable(true);
-            
-        for row in 0..GRID_SIZE {
-            for col in 0..GRID_SIZE {
-                grid.end();
-                self.display_button(row, col);
-            }
-        }
-        grid.end();
+        
+        self.display_play_grid(); 
         self.display_control_panel();
 
         window.make_resizable(true);
@@ -47,6 +39,24 @@ impl Grid{
         app.run().unwrap();
     }
 
+    fn display_play_grid(&mut self) {
+        let mut grid = Pack::new(
+            10,
+            10,
+            BUTTON_SIZE * GRID_SIZE as i32,
+            BUTTON_SIZE * GRID_SIZE as i32,
+            "");
+        grid.make_resizable(true);
+            
+        for row in 0..GRID_SIZE {
+            for col in 0..GRID_SIZE {
+                grid.end();
+                self.display_button(row, col);
+            }
+        }
+        grid.end();
+    }
+
     fn display_control_panel(&mut self) {
         for number in 0..9 {
             self.control_panel.borrow_mut()[number] = Button::new(
@@ -54,11 +64,15 @@ impl Grid{
                 BUTTON_SIZE * GRID_SIZE as i32 + 50,
                 BUTTON_SIZE,
                 BUTTON_SIZE,
-                "1",
+                "",
             );
             self.control_panel.borrow_mut()[number].set_label(&format!("{}", number + 1));
-            self.control_panel.borrow_mut()[number].set_color(Color::White.darker());
+            self.control_panel.borrow_mut()[number].set_color(LIGHT_BUTTON_COLOR);
         }
+        self.set_control_panel_callbacks();
+    }
+
+    fn set_control_panel_callbacks(&mut self) {
         for control_button in self.control_panel.borrow_mut().iter_mut() {
             let current_number_clone = Rc::clone(&self.current_number);
             let control_panel_clone = Rc::clone(&self.control_panel);
@@ -66,32 +80,37 @@ impl Grid{
             control_button.set_callback(move |button: &mut Button| {
                 *current_number_clone.borrow_mut() = button.label().to_string();
                 for but in (*control_panel_clone.borrow_mut()).iter_mut() {
-                    but.set_color(Color::White.darker());
+                    but.set_color(LIGHT_BUTTON_COLOR);
                     but.redraw();
                 }
-                button.set_color(Color::Blue.lighter());
+                button.set_color(HIGHLIGHTED_BUTTON_COLOR);
             });
         }
     }
 
     fn display_button(&self, row: usize, col: usize) {
-        let mut button = Button::new(
+        self.play_grid.borrow_mut()[row][col] = Button::new(
             GRID_OFFSET_FROM_LEFT + col as i32 * BUTTON_SIZE,
             30 + row as i32 * BUTTON_SIZE,
             BUTTON_SIZE,
             BUTTON_SIZE,
-            "1",
+            "",
         );
-        button.set_label("");
 
+        self.set_play_button_callback(row, col);
+        self.show_squares(row, col);
+    }
+
+    fn set_play_button_callback(&self, row: usize, col: usize) {
         let button_label = Rc::clone(&self.current_number);
-            button.set_callback(move |button: &mut Button| {
+        self.play_grid.borrow_mut()[row][col].set_callback(move |button: &mut Button| {
             button.set_label(&format!("{}", button_label.borrow()));
         });
+    }
 
+    fn show_squares(&self, row: usize, col: usize) {
         let square_id = (row / 3) + (col / 3);
-        if square_id % 2 == 1 {
-            button.set_color(button.color().lighter());
-        }
+        let button_color = if square_id % 2 == 1 {DARK_BUTTON_COLOR} else {LIGHT_BUTTON_COLOR};
+        self.play_grid.borrow_mut()[row][col].set_color(button_color);
     }
 }
