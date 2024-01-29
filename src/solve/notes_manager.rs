4ceb71e40c833097a93fc8f53a93f5c19c6f7da5
Aleksander Augustyniak <aleksander.z.augustyniak@gmail.com>
2024-01-29 11:49:35 +0100

    Simplify notes initial fill

--- src/solve/notes_manager.rs
+++ src/solve/notes_manager.rs
@@ -3,16 +3,12 @@ use crate::common::puzzle::*;
 use crate::solve::coordinates::*;
 use crate::solve::notes::Notes;
 
-type HelpNotes = [u16; GRID_SIZE];
 #[allow(clippy::unusual_byte_groupings)]
 const FILLED_BITSET: u16 = 0b111_111_111; // Group by 3 fits better to the project purpose
 
 pub struct NotesManager {
     puzzle: Puzzle,
     notes: Notes,
-    row_notes: HelpNotes,
-    col_notes: HelpNotes,
-    square_notes: HelpNotes, //TODO: consider remowing those fields
 }
 
 impl NotesManager {
@@ -21,104 +17,58 @@ impl NotesManager {
         NotesManager {
             puzzle: play_board,
             notes,
-            row_notes: Default::default(),
-            col_notes: Default::default(),
-            square_notes: Default::default(),
         }
     }
 
     pub fn fill(&mut self) {
-        self.reset();
         for (row, notes_row) in self.notes.iter_mut().enumerate() {
             for (col, note) in notes_row.iter_mut().enumerate() {
                 *note = if self.puzzle[row][col] != 0 { 0 } else { FILLED_BITSET };
             }
         }
-        for i in 0..GRID_SIZE {
-            self.set_row_based_notes(i);
-            self.set_column_based_notes(i);
+        for index in 0..GRID_SIZE {
+            self.set_notes(&get_row_coordinates(index));
+            self.set_notes(&get_col_coordinates(index));
+            self.set_notes(&get_square_coordinates((index / 3, index % 3)));
         }
-        self.set_squares_based_notes();
     }
 
     pub fn get(&self) -> Notes {
         self.notes
     }
 
-    fn set_row_based_notes(&mut self, row: usize) {
-        for cell in self.puzzle[row].iter() {
-            if *cell == 0 {
-                continue;
-            }
-            Self::unset_note(&mut self.row_notes[row], *cell);
-        }
-        for note in self.notes[row].iter_mut() {
-            *note &= self.row_notes[row];
-        }
-    }
-
-    fn set_column_based_notes(&mut self, col: usize) {
-        for row in self.puzzle.iter() {
-            if row[col] == 0 {
-                continue;
-            }
-            Self::unset_note(&mut self.col_notes[col], row[col]);
-        }
-        for row in self.notes.iter_mut() {
-            row[col] &= self.col_notes[col];
-        }
-    }
-
-    fn set_squares_based_notes(&mut self) {
-        for square_x in 0..3 {
-            for square_y in 0..3 {
-                self.set_square_notes(square_x, square_y);
-            }
-        }
-        for (row, notes_row) in self.notes.iter_mut().enumerate() {
-            for (col, note) in notes_row.iter_mut().enumerate() {
-                if self.puzzle[row][col] != 0 {
-                    continue;
-                }
-                *note &= self.square_notes[Self::get_square_index(row, col)];
+    fn set_notes(&mut self, coordinates: &Coordinates) {
+        let note = self.get_notes(coordinates);
+        for (row, col) in coordinates.iter() {
+            if self.puzzle[*row][*col] == 0 {
+                self.notes[*row][*col] &= note;
             }
         }
     }
 
-    fn set_square_notes(&mut self, x: usize, y: usize) {
-        for (row, col) in get_square_coordinates((x, y)).iter() {
+    fn get_notes(&mut self, coordinates: &Coordinates) -> u16 {
+        let mut note: u16 = FILLED_BITSET;
+        for (row, col) in coordinates.iter() {
             if self.puzzle[*row][*col] == 0 {
                 continue;
             }
-            Self::unset_note(&mut self.square_notes[3 * x + y], self.puzzle[*row][*col]);
+            Self::unset_note(&mut note, self.puzzle[*row][*col]);
         }
-    }
-
-    fn reset(&mut self) {
-        for i in 0..GRID_SIZE {
-            self.row_notes[i] = FILLED_BITSET;
-            self.col_notes[i] = FILLED_BITSET;
-            self.square_notes[i] = FILLED_BITSET;
-        }
+        note
     }
 
     pub fn adjust(&mut self, row: usize, col: usize, value: u8) {
         for cell in self.notes[row].iter_mut() {
             Self::unset_note(cell, value);
         }
-        Self::unset_note(&mut self.row_notes[row], value);
 
         for row_pos in self.notes.iter_mut() {
             Self::unset_note(&mut row_pos[col], value);
         }
-        Self::unset_note(&mut self.col_notes[col], value);
 
         for (x, y) in get_square_coordinates((row / 3, col / 3)) {
             Self::unset_note(&mut self.notes[x][y], value);
         }
-        let square_index = Self::get_square_index(row, col);
-        Self::unset_note(&mut self.square_notes[square_index], value);
-
         self.notes[row][col] = 0;
     }
 
@@ -130,10 +80,6 @@ impl NotesManager {
     fn is_set(note: &u16, position: usize) -> bool {
         let mask = 1 << (position - 1);
         (*note & mask) != 0
-    }
-
-    fn get_square_index(row: usize, col: usize) -> usize {
-        3 * (row / 3) + col / 3
     }
 
     pub fn set_obvious_pairs(&mut self) -> bool {
